b3a2713397690432428b825bee432aee4ae7427e
Hongjia Liu <hongjia_liu@outlook.com>
2019-10-12 23:20:48 +0100

    Finish Assignment 3 Part 2

--- Assignment 3/src/Student.java
+++ Assignment 3/src/Student.java
@@ -1,40 +1,71 @@
-import java.util.Scanner;
-import java.text.DecimalFormat;
-
 /**
- * Student.java - CT874 Assignment 3 Part 2
+ * Student.java - CT874 Assignment 3 Part 2-1
  * @author Hongjia Liu 19234417
  */
 
+/**
+ * Implement the Student class.
+ */
 public class Student {
     private String name;
     private long IDnumber;
 
-
+    /**
+     * Constructor
+     */
     public Student() {
+        name = "unassigned";
+        IDnumber = 0;
     }
 
+    /**
+     * Constructor
+     * @param name
+     * @param IDnumber
+     */
     public Student(String name, long IDnumber) {
-
+        setName(name);
+        setIDnumber(IDnumber);
     }
 
+    /**
+     *
+     * @return String representation of the Student object
+     */
     @Override
     public String toString() {
-        return super.toString();
+        return "name: " + getName() +
+                " IDnumber: " + getIDnumber();
     }
 
+    /**
+     *
+     * @return student name
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     *
+     * @param name new student name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     *
+     * @return student ID number
+     */
     public long getIDnumber() {
         return IDnumber;
     }
 
+    /**
+     *
+     * @param IDnumber new student ID number
+     */
     public void setIDnumber(long IDnumber) {
         this.IDnumber = IDnumber;
     }
--- Assignment 3/src/StudentTester.java
+++ Assignment 3/src/StudentTester.java
@@ -1,5 +1,25 @@
+/**
+ * StudentTester.java - CT874 Assignment 3 Part 2-2
+ * @author Hongjia Liu 19234417
+ */
+
+/**
+ *  A tester class for student class
+ */
 public class StudentTester {
     public static void main(String[] args) {
+        // create an array to hold a collection of Student objects.
+        Student[] arr = new Student[4];
+        // add a number of instances of Student to the array.
+        arr[0] = new Student("Hongjia Liu", 19234417);
+        arr[1] = new Student("LeBron James", 15548643);
+        arr[2] = new Student("Michael Jordan", 1365042);
+        arr[3] = new Student("Stephen Curry", 18619462);
 
+        // a loop traverses the array and displays the contents.
+        for (int i = 0; i < 4; i++) {
+            System.out.print("Student " + (i + 1) + " ");
+            System.out.println(arr[i]);
+        }
     }
 }

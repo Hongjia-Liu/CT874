cb382824f511e4e7dcb5bb4267f0f1102c1b9031
Hongjia Liu <hongjia_liu@outlook.com>
2019-10-21 19:40:10 +0100

    Complete Assignment 4

--- Assignment 4/src/ArrayListApp.java
+++ Assignment 4/src/ArrayListApp.java
@@ -1,2 +1,71 @@
-// ArrayList Application
-// available after 15:00 October 21, 2019
+/**
+ * ArrayListApp.java - CT874 Assignment 4 Part 2
+ * @author Hongjia Liu 19234417
+ */
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class ArrayListApp {
+    public static void main(String[] args) {
+        System.out.print("Enter size of the ArrayList: ");
+        Scanner input = new Scanner(System.in);
+        // get list size from user
+        Integer listSize = input.nextInt(); // Autoboxing taking place
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        // standard for loop to populate the ArrayList
+        // using  Math.random() method
+        for (int i = 0; i < listSize; i++) { // Auto-unboxing
+            int element = (int)(10 * Math.random());
+            list.add(element); // Autoboxing taking place
+        }
+
+        // enhanced for loop to traverse the array
+        // and display the contents of the ArrayList.
+        System.out.print("Display the contents of the ArrayList: ");
+        for (Integer element: list) {
+            System.out.print(element + " ");
+        }
+        System.out.println(" ");
+
+        // Remove an element from the ArrayList
+        System.out.print("Remove the first element of the ArrayList: ");
+        list.remove(0);
+        // an enhanced for loop to display the contents.
+        for (Integer element: list) {
+            System.out.print(element + " ");
+        }
+        System.out.println(" ");
+
+        System.out.print("Enter size of the LinkedList: ");
+        input = new Scanner(System.in);
+        // get list size from user
+        listSize = input.nextInt(); // Autoboxing taking place
+        LinkedList<Integer> linkedList = new LinkedList<Integer>();
+
+        // standard for loop to populate the LinkedList
+        // using  Math.random() method
+        for (int i = 0; i < listSize; i++) { // Auto-unboxing
+            int element = (int)(10 * Math.random());
+            linkedList.add(element); //  Autoboxing taking place
+        }
+
+        // enhanced for loop to traverse the list
+        // and display the contents of the LinkedList.
+        System.out.print("Display the contents of the LinkedList: ");
+        for (Integer element: linkedList) {
+            System.out.print(element + " ");
+        }
+        System.out.println(" ");
+
+        // Remove an element from the LinkedList
+        System.out.print("Remove the first element of the LinkedList: ");
+        linkedList.remove(0);
+        // an enhanced for loop to display the contents.
+        for (Integer element: linkedList) {
+            System.out.print(element + " ");
+        }
+        System.out.println(" ");
+    }
+}
--- Assignment 4/src/TemperatureApp.java
+++ Assignment 4/src/TemperatureApp.java
@@ -1,2 +1,79 @@
-// Temperature Application
-// available after 15:00 October 21, 2019
+/**
+ * TemperatureApp.java - CT874 Assignment 4 Part 1
+ * @author Hongjia Liu 19234417
+ */
+
+import java.util.Scanner;
+
+public class TemperatureApp
+{
+    /**
+     * a Celsius method which returns the Celsius equivalent
+     * of a Fahrenheit temperature
+     * @param fahrenheit a Fahrenheit temperature
+     * @return the Celsius equivalent of Fahrenheit temperature
+     */
+    public static double Celsius(double fahrenheit) {
+        return 5.0 / 9.0 * (fahrenheit - 32);
+    }
+
+    /**
+     * a Fahrenheit method, which returns the Fahrenheit equivalent
+     * of a Celsius temperature
+     * @param celsius a Celsius temperature
+     * @return the Fahrenheit equivalent of Fahrenheit temperature
+     */
+    public static double Fahrenheit(double celsius) {
+        return 9.0 / 5.0 * celsius + 32;
+    }
+
+    /**
+     * Show Prompt message and return user's choice of operation
+     * @return integer representation of user's choice
+     */
+    public static int getChoiceFromUser() {
+        System.out.println("1. Fahrenheit to Celsius");
+        System.out.println("2. Celsius to Fahrenheit");
+        System.out.println("3. Exit");
+        System.out.print("Choice: ");
+        Scanner choiceInput = new Scanner(System.in);
+        return choiceInput.nextInt();
+    }
+
+    /**
+     * Show Prompt message and return temperature entered by user
+     * @return temperature entered by user
+     */
+    public static double getTemperatureFromUser() {
+        System.out.print("Enter temperature: ");
+        Scanner temperatureInput = new Scanner(System.in);
+        return temperatureInput.nextDouble();
+    }
+
+    public static void main(String[] args) {
+        while (true) {
+            // get choice from user
+            int choice = getChoiceFromUser();
+
+            // if user input 1, convert Fahrenheit to Celsius
+            if (choice == 1) {
+                double temperature = getTemperatureFromUser();
+                System.out.println((int)temperature + " Fahrenheit is " +
+                        (int)Celsius(temperature) + " Celsius\n");
+            // if user input 2, convert Celsius to Fahrenheit
+            }else if (choice == 2) {
+                double temperature = getTemperatureFromUser();
+                System.out.println((int)temperature + " Celsius is " +
+                        (int)Fahrenheit(temperature)+ " Fahrenheit\n");
+            // if user input 3, Terminate the program
+            }else if (choice == 3){
+                break;
+            // other inputs are illegal
+            } else {
+                throw new IllegalArgumentException(
+                        "Choice should be 1, 2 or 3");
+            }
+        }
+        System.out.println("Program Terminated\n");
+    }
+}
